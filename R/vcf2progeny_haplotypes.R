6cdb2843279651553d5b5cef566d5c61d76a740e
cristianetaniguti <chaytaniguti@gmail.com>
2021-10-11 17:10:36 -0500

    update haplo output

--- R/vcf2progeny_haplotypes.R
+++ R/vcf2progeny_haplotypes.R
@@ -21,52 +21,52 @@ vcf2progeny_haplotypes <- function(vcfR.object,
   n.mk <- dim(vcfR.object@gt)[1]
   n.ind <- dim(vcfR.object@gt)[2]-1
   INDS <- dimnames(vcfR.object@gt)[[2]][-1]
-
+  
   if(length(which(INDS %in% ind.id)) != length(ind.id)){
     stop("At least one of the individuals in ind.id was not found in vcfR object.")
   }
-
+  
   MKS <- vcfR.object@fix[,3]
   if (any(MKS == "." | is.na(MKS))) MKS <- paste0(vcfR.object@fix[,1],"_", vcfR.object@fix[,2])
-
+  
   # Geno matrix
   GT_matrix <- matrix(rep(NA,n.ind*n.mk), ncol=n.ind, nrow=n.mk)
   GT <- which(strsplit(vcfR.object@gt[1,1], split=":")[[1]]=="GT")
-
+  
   for(i in 2:(n.ind+1))
     GT_matrix[,i-1] <- unlist(lapply(strsplit(vcfR.object@gt[,i], split=":"), "[[", GT))
-
+  
   CHROM <- vcfR.object@fix[,1]
-
+  
   if(is.null(group_names)) group_names <- CHROM[1]
-
+  
   if(length(which(unique(CHROM) %in% group_names)) != length(group_names)){
     stop("At least one of the groups in group_names was not found in vcfR object.")
   }
-
+  
   progeny_haplotypes_obj_chr <- data.frame()
   for(chr in 1:length(group_names)){ ### Need optimization
     CHROM.now <- which(CHROM %in% group_names[chr])
     POS <- as.numeric(vcfR.object@fix[,2])[CHROM.now]
-
+    
     colnames(GT_matrix) <- INDS
     rownames(GT_matrix) <- MKS
-
+    
     P1.idx <- grep(parent1, INDS)
     P2.idx <- grep(parent2, INDS)
-
+    
     P1_1 <- sapply(strsplit(GT_matrix[CHROM.now,P1.idx], "[|]"), "[",1)
     P1_2 <- sapply(strsplit(GT_matrix[CHROM.now,P1.idx], "[|]"), "[",2)
     P2_1 <- sapply(strsplit(GT_matrix[CHROM.now,P2.idx], "[|]"), "[",1)
     P2_2 <- sapply(strsplit(GT_matrix[CHROM.now,P2.idx], "[|]"), "[",2)
-
+    
     progeny_haplotypes_obj_ind <- data.frame()
     for(ind in 1:length(ind.id)){
       ind.idx <- grep(ind.id[ind], INDS)
       ind.number <- grep(ind.id[ind], INDS[-c(P1.idx, P2.idx)])
       ind_1 <- sapply(strsplit(GT_matrix[CHROM.now,ind.idx], "[|]"), "[",1)
       ind_2 <- sapply(strsplit(GT_matrix[CHROM.now,ind.idx], "[|]"), "[",2)
-
+      
       p.names <- c("H1", "H2", "H1", "H2")
       Hs <-  rep(list(rep(NA, length(ind_1))),2)
       progeny_haplotypes_obj <- data.frame()
@@ -106,7 +106,7 @@ vcf2progeny_haplotypes <- function(vcfR.object,
               inter.cum <- inter.cum + 1
               ref.frags.new[ref.frags==idx.cum][1:(frag[1]-1)] <- inter.cum
             }
-
+            
             if(!is.null(frag2)){
               new.zs <- c(new.zs,list(frag2))
               inter.cum <- inter.cum + 1
@@ -117,7 +117,7 @@ vcf2progeny_haplotypes <- function(vcfR.object,
           }
           comp <- new.zs
         }
-
+        
         num.mk <- length(CHROM.now)
         df.H <- data.frame(ind = rep(ind.id[ind], 2*num.mk),
                            grp = rep(CHROM[CHROM.now], 2),
@@ -125,7 +125,7 @@ vcf2progeny_haplotypes <- function(vcfR.object,
                            prob = rep(0, 2*num.mk),
                            parents = rep(c("P1", "P2")[w], 2*num.mk),
                            homologs = rep(c("H1","H2"), each=num.mk))
-
+        
         for(i in 1:length(Hs[[w]])){
           df.H$prob[which(df.H$pos == POS[i] & df.H$homologs == Hs[[w]][i])] <- 1
         }
@@ -138,14 +138,16 @@ vcf2progeny_haplotypes <- function(vcfR.object,
     # bind chromosomes
     progeny_haplotypes_obj_chr <- rbind(progeny_haplotypes_obj_chr,progeny_haplotypes_obj_ind)
   }
-
+  
   crosstype <- switch(crosstype, "outcross" = "outcross", "f2 intercross"="f2",
                       "f2 backcross"="backcross", "ril sib"="rils", "ril self"="rils")
-
+  
   flag <- "most.likely"
   progeny_haplotypes_obj_chr$marker <- rep(MKS,4)
   progeny_haplotypes_obj_chr <- progeny_haplotypes_obj_chr[, c("ind", "marker","grp", "pos", "prob", "parents", "homologs")]
-
+  progeny_haplotypes_obj_chr$allele <- paste0(progeny_haplotypes_obj_chr$parents, "_",progeny_haplotypes_obj_chr$homologs)
+  colnames(progeny_haplotypes_obj_chr)[7] <- "parents.homologs"
+  
   class(progeny_haplotypes_obj_chr) <- c("onemap_progeny_haplotypes", crosstype, "data.frame", flag)
   return(progeny_haplotypes_obj_chr)
 }
