ec8340a7754bab0ad33e913f3d09d9b9c574eb95
cristianetaniguti <chaytaniguti@gmail.com>
2021-10-22 15:09:17 -0500

    func man

--- R/get_HPC_info.R
+++ R/get_HPC_info.R
@@ -1,12 +1,70 @@
+#' Get RAM and CPU usage from SLURM managment system
+#' 
+#' 
+#'
+#' @import lubridate
+#' @import ggplot2
+#' @import tidyr
+#' @import dplyr
+#' @import ggpubr
+#' 
+#' @examples 
+#' \dontrun{
+#'   # grep -rw "Submitted batch job" * > jobs
+#'   df <- read.table("/scratch/user/chtaniguti/cromwell-executions/SNPCalling/7cefff16-8d18-46d7-adeb-b22e4f35b663/jobs")
+#'   df <- read.table("jobs_acca")
+#'   tasks <- sapply(strsplit(df$V1, "/"), function(x) paste0(x[grep("call-", x)], collapse = "/"))
+#'   tasks <- gsub("call-", "", tasks)
+#'   jobs_acca <- data.frame(tasks, df$V4)
+#'   write.table(df$V4, quote = F, file = "jobs_acca_id", col.names = F, row.names = F)
+#' 
+#'   df <- read.table("jobs_euc") 
+#'   tasks <- sapply(strsplit(df$V1, "/"), function(x) paste0(x[grep("call-", x)], collapse = "/"))
+#' 
+#'   tasks <- gsub("call-", "", tasks)
+#'   jobs_euc <- data.frame(tasks, df$V4)
+#'   write.table(df$V4, quote = F, file = "jobs_euc_id", col.names = F, row.names = F)
+#' 
+#'   # for i in $(cat jobs_euc_id);do
+#'   #   seff $i >> infos.temp.euc
+#'   # done
+#' 
+#'   df.acca <- efficiency_table(infos_seff = "infos.temp.acca", jobs_acca)
+#'   efficiency_graphics(df.acca, out_name = "acca_efficiency_wf.png")
+#' 
+#'   df.euc <- efficiency_table(infos_seff = "infos.temp.euc", jobs_euc)
+#'   efficiency_graphics(df.euc, out_name = "euc_efficiency_wf.png")
+#' }
+#' 
+#' @export
+efficiency_graphics <- function(eff_table, out_name){
+  perc <- eff_table %>% 
+    mutate(mem = (mem_used_GB/mem_GB)*100 , cpu = (cpu_used/cpu)*100) %>% 
+    select(tasks, mem, cpu) %>% pivot_longer(cols = c(2:3), names_to="Usage (%)", values_to = "percentage") %>%
+    ggplot(aes(x=tasks, y=percentage, fill=`Usage (%)`, color = `Usage (%)`)) +   coord_flip() +
+    geom_boxplot()  + theme_bw()
+  
+  mem <- eff_table %>%  
+    select(tasks, mem_used_GB) %>% pivot_longer(cols = c(2), values_to="usage (GB)") %>%
+    ggplot(aes(x=tasks, y=`usage (GB)`)) +   coord_flip() +
+    geom_boxplot()  + ylab("usage mem (GB)")  + theme_bw() 
+  
+  cpu <- eff_table %>%  
+    select(tasks, cpu_used) %>% pivot_longer(cols = c(2), values_to = "usage (min)") %>%
+    ggplot(aes(x=tasks, y = `usage (min)`/60)) +   coord_flip() +
+    geom_boxplot() + ylab("usage cpus (min)") + theme_bw() +
+    theme(axis.title.y=element_blank(),
+          axis.text.y=element_blank(),
+          axis.ticks.y=element_blank()) 
+  
+  p <- ggarrange(mem, cpu, nrow = 1)
+  (p2 <- ggarrange(p, perc, nrow = 2))
+  
+  ggsave(p2, filename = out_name, width = 10, height = 7)
+}
 
-# Packages
-library(lubridate)
-library(ggplot2)
-library(tidyr)
-library(dplyr)
-library(ggpubr)
 
-# Functions
+
 transform_day <- function(x){
   day <- as.numeric(x[1])*24
   split.t <- strsplit(x[2], ":")
@@ -14,6 +72,7 @@ transform_day <- function(x){
   paste0(new.h, ":",sapply(split.t, "[",2),":",sapply(split.t, "[",3))
 }
 
+#' @export
 efficiency_table <- function(infos_seff, jobs){
   
   df <- read.table(infos_seff, sep = "\n")
@@ -65,55 +124,7 @@ efficiency_table <- function(infos_seff, jobs){
   return(df.infos)
 }
 
-efficiency_graphics <- function(eff_table, out_name){
-  perc <- eff_table %>% 
-    mutate(mem = (mem_used_GB/mem_GB)*100 , cpu = (cpu_used/cpu)*100) %>% 
-    select(tasks, mem, cpu) %>% pivot_longer(cols = c(2:3), names_to="Usage (%)", values_to = "percentage") %>%
-    ggplot(aes(x=tasks, y=percentage, fill=`Usage (%)`, color = `Usage (%)`)) +   coord_flip() +
-    geom_boxplot()  + theme_bw()
-  
-  mem <- eff_table %>%  
-    select(tasks, mem_used_GB) %>% pivot_longer(cols = c(2), values_to="usage (GB)") %>%
-    ggplot(aes(x=tasks, y=`usage (GB)`)) +   coord_flip() +
-    geom_boxplot()  + ylab("usage mem (GB)")  + theme_bw() 
-  
-  cpu <- eff_table %>%  
-    select(tasks, cpu_used) %>% pivot_longer(cols = c(2), values_to = "usage (min)") %>%
-    ggplot(aes(x=tasks, y = `usage (min)`/60)) +   coord_flip() +
-    geom_boxplot() + ylab("usage cpus (min)") + theme_bw() +
-    theme(axis.title.y=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank()) 
-  
-  p <- ggarrange(mem, cpu, nrow = 1)
-  (p2 <- ggarrange(p, perc, nrow = 2))
-  
-  ggsave(p2, filename = out_name, width = 10, height = 7)
-}
-
-# grep -rw "Submitted batch job" * > jobs
-# df <- read.table("/scratch/user/chtaniguti/cromwell-executions/SNPCalling/7cefff16-8d18-46d7-adeb-b22e4f35b663/jobs")
-
-df <- read.table("jobs_acca")
-tasks <- sapply(strsplit(df$V1, "/"), function(x) paste0(x[grep("call-", x)], collapse = "/"))
-tasks <- gsub("call-", "", tasks)
-jobs_acca <- data.frame(tasks, df$V4)
-write.table(df$V4, quote = F, file = "jobs_acca_id", col.names = F, row.names = F)
 
 
-df <- read.table("jobs_euc") 
-tasks <- sapply(strsplit(df$V1, "/"), function(x) paste0(x[grep("call-", x)], collapse = "/"))
-tasks <- gsub("call-", "", tasks)
-jobs_euc <- data.frame(tasks, df$V4)
-write.table(df$V4, quote = F, file = "jobs_euc_id", col.names = F, row.names = F)
 
-# for i in $(cat jobs_euc_id);do
-#   seff $i >> infos.temp.euc
-# done
-
-df.acca <- efficiency_table(infos_seff = "infos.temp.acca", jobs_acca)
-efficiency_graphics(df.acca, out_name = "acca_efficiency_wf.png")
-
-df.euc <- efficiency_table(infos_seff = "infos.temp.euc", jobs_euc)
-efficiency_graphics(df.euc, out_name = "euc_efficiency_wf.png")
 
